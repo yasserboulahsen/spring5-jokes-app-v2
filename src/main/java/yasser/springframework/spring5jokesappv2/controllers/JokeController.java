848b911cc3971d61c7a508db368d616b29cf0446
Boulahsen, Yasser <boulahsen.yasser@courrier.uqam.ca>
2022-07-21 19:15:20 -0400

    joke html index page added

---
+++ src/main/java/yasser/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package yasser.springframework.spring5jokesappv2.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import yasser.springframework.spring5jokesappv2.services.JokeService;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return "index";
+    }
+}
---
+++ src/main/resources/templates/script.js
@@ -0,0 +1,8 @@
+
+"use strict";
+
+const btn = document.querySelector(".showJokes");
+
+btn.addEventListener("click",()=>{
+    console.log("show jokes");
+})
